f245ddb0bcc512d58ed0d58578f5acdfdb44fcc4
PrakharPandeyyy <8.prakharpandey@gmail.com>
2023-03-04 11:33:58 +0530

    Added all Functionalities , now working on managing Database

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ =require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,59 +11,59 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts=[];
+let posts = [];
 
-app.get("/",function(req,res){
-  res.render('home',{
-    startcontent:homeStartingContent,
-    posts :posts
+app.get("/", function (req, res) {
+  res.render('home', {
+    startcontent: homeStartingContent,
+    posts: posts
   });
 });
 
-app.get("/contact",function(req,res){
-  res.render('contact',{
-    contact:contactContent
+app.get("/contact", function (req, res) {
+  res.render('contact', {
+    contact: contactContent
   });
 });
 
-app.get("/about",function(req,res){
-  res.render('about',{
-    about:aboutContent
+app.get("/about", function (req, res) {
+  res.render('about', {
+    about: aboutContent
   });
 });
 
-app.get("/compose",function(req,res){
+app.get("/compose", function (req, res) {
   res.render('compose');
 });
 
-app.post("/compose",function(req,res){
-  let post ={
-    ptitle:req.body.postTitle,
-    pbody:req.body.postBody
+app.post("/compose", function (req, res) {
+  let post = {
+    pTitle: req.body.postTitle,
+    pBody: req.body.postBody
   };
   posts.push(post);
-  
+
   res.redirect("/");
+
+});
+app.get("/posts/:postName",function(req,res){
+    const requestedTitle=_.lowerCase(req.params.postName);
+    posts.forEach(function(post){
+      const storedTitle=post.pTitle;
+      if(requestedTitle === storedTitle){
+        res.render('post',{
+          poTitle:requestedTitle,
+          poBody:post.pBody
+        });
+      }
+    });
 
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
